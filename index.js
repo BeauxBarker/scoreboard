4a4c063b1dda7844a281c78761e05afb5d0acdc7
B Barker <33534620+BeauxBarker@users.noreply.github.com>
2023-01-27 15:41:34 +0000

    commet saved score

--- index.js
+++ index.js
@@ -6,22 +6,24 @@ let scoreA = 0
 let homeValue = [ ]
 
 
+
+
 function add1H(){
     home.textContent = scoreH += 1
     homeValue.push(scoreH)
-    return scoreH
+    
 }
 
 function add2H(){
     home.textContent = scoreH += 2
     homeValue.push(scoreH)
-    return scoreH
+    
 }
 
 function add3H(){
     home.textContent = scoreH += 3
     homeValue.push(scoreH)
-    return scoreH
+    
 }
 
 
@@ -39,17 +41,19 @@ function add3A(){
 
 
 
-
+let savedScore = document.getElementById("home--last--score")
 let period = document.getElementById("period")
 let perNum = 1
-let hScore = document.getElementById("home-last-score")
+let sum = 0
+
 
 
 function startTimer(duration, display) {
-    
-    
+   
     var timer = duration, minutes, seconds;
     setInterval(function () {
+       
+
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
@@ -59,14 +63,31 @@ function startTimer(duration, display) {
         display.textContent = minutes + ":" + seconds;
 
         if (--timer < 0) {
-            let newValue = homeValue.slice(-1)
+           
             timer = duration;
-            period.textContent = perNum += 1
-            hScore.textContent = newValue 
-            home.textContent = timer - 30
+            period.textContent = perNum += 1;
+            home.textContent = timer - 15;
+     
+            for (const [i, value] of homeValue.entries()) {
+                if (i === homeValue.length - 1) {
+                    
+
+                    sum += homeValue[i]
+                    savedScore.textContent = sum
+                    homeValue[i] = 0
+                    
+                 
+                }
+            } 
+           
+            console.log(sum)
+            
+        
             scoreH = 0
-            console.log(scoreH)
+            
         }
+       
+        
         
         
         
@@ -74,15 +95,15 @@ function startTimer(duration, display) {
             console.log("winner!")
            
         }
-              
-
+            
+  
+        
     }, 1000);
 }
 
 window.onload = function () {
-    var fiveMinutes = 30 * 1,
+    var fiveMinutes = 15 * 1,
         display = document.querySelector('#time');
     startTimer(fiveMinutes, display);
 };
-
 
